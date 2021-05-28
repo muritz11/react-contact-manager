d5f1191d627ba5829ddd123c04d71983d3d1b03d
muritz <mauricechisom275@gmail.com>
2021-05-28 15:57:21 +0100

    added/styled the header, made app responsive

--- src/App.js
+++ src/App.js
@@ -63,10 +63,14 @@ class AddContact extends React.Component{
 
   render(){
     return (
-      <form onSubmit={this.handleSubmit} className='form-inline ml-5 p-3'>
-        <input type='text' placeholder='Add new contact' onChange={this.handleInput} value={this.state.newPerson}  className="form-control col-8 mr-3" />
-        <button type='submit' className="btn btn-info form-control col-1">Add</button>
-      </form>
+      <div className="jumbotron pb-4">
+        <h3>Contact manager</h3>
+
+        <form onSubmit={this.handleSubmit} className='form-inline ml-sm-5 mt-5'>
+        <input type='text' placeholder='Add new contact' onChange={this.handleInput} value={this.state.newPerson}  className="form-control col-sm-8 mr-3" />
+        <button type='submit' className="btn btn-info col-sm-1 col-8 mt-sm-0 mt-2">Add</button>
+        </form>
+      </div>
     );
   }
 }
